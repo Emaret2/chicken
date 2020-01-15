059f660ef2ae159362a64929735d2d7c49f7890d
Elijah Maret <Emaret2@mail.greenriver.edu>
2020-01-15 14:04:11 -0500

    added index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * Elijah Maret
+ * IT 328
+ * 1/15/2020
+ * http://emaret.greenriverdev.com/328/chicken/
+ * Chicken
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+
